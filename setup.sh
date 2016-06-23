c18a7fe57b9820c94a7ececdb91be9c1c13d867c
tokamoto6676 <tokamoto6676+cloud9@gmail.com>
2016-06-23 11:45:01 +0000

    ok

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ wget https://services.gradle.org/distributions/gradle-2.14-all.zip
 unzip gradle-2.14-all.zip
 mv gradle-2.14 ../
 
-echo SCALA_HOME="$HOME/scala-2.11.5" >> ~/.profile
-echo GRADLE_HOME="$HOME/gradle-2.14" >> ~/.profile
-echo PATH="$PATH:$SCALA_HOME/bin:$GRADLE_HOME/bin" >> ~/.profile
+SCALA_HOME="$HOME/scala-2.11.5"
+GRADLE_HOME="$HOME/gradle-2.14"
+PATH="$PATH:$SCALA_HOME/bin:$GRADLE_HOME/bin"
 
 gradle init --type scala-library
+mv hello-world.scala src/main/scala/
+mv build.gradle.tmp build.gradle
